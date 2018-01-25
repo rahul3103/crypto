451941109954978a69265ed2dbbc5890a098081c
Rahul Shrivastava <rahul3103@gmail.com>
2018-01-26 01:23:28 +0530

    requirements for heroku

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 import json
 from flask import Flask, jsonify, request, render_template
-from playhouse.shortcuts import model_to_dict
+# from playhouse.shortcuts import model_to_dict
 from flask_cors import CORS
-from model import Crypto
-from peewee import create_model_tables
+# from model import Crypto
+# from peewee import create_model_tables
 from extractors import data_collector
 
 app = Flask(__name__)
 CORS(app)
 
-create_model_tables([Crypto], fail_silently=True)
+# create_model_tables([Crypto], fail_silently=True)
 
-@app.route('/api/crypto/<coin>/<period>', methods=['GET'])
-def users(coin, period):
-    if request.method == 'GET':
-        coins = Crypto.select().where(Crypto.coin == coin).order_by(Crypto.cdate.desc())
-        if coins:
-            if period == 'past':
-                return (jsonify(coins=[coin.to_dict([Crypto.id, Crypto.coin]) for coin in coins])), 200
-            if period == 'current':
-                coins =  coin_price()
-                for ccoin in coins:
-                    if ccoin['name'] == coin:
-                        return jsonify({ coin: ccoin['value']}), 200
-
-        return json.dumps({'Error': coin + ' Cryptocoin not added yet or period is not corrrect'}), 400
+# @app.route('/api/crypto/<coin>/<period>', methods=['GET'])
+# def users(coin, period):
+#     if request.method == 'GET':
+#         coins = Crypto.select().where(Crypto.coin == coin).order_by(Crypto.cdate.desc())
+#         if coins:
+#             if period == 'past':
+#                 return (jsonify(coins=[coin.to_dict([Crypto.id, Crypto.coin]) for coin in coins])), 200
+#             if period == 'current':
+#                 coins =  coin_price()
+#                 for ccoin in coins:
+#                     if ccoin['name'] == coin:
+#                         return jsonify({ coin: ccoin['value']}), 200
+#
+#         return json.dumps({'Error': coin + ' Cryptocoin not added yet or period is not corrrect'}), 400
 
 @app.route('/')
 def index():
@@ -32,4 +32,4 @@ def index():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
